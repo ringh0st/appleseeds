4b9490a740619c7702cc720fbf8f79066b2f8455
Rinat Nadav <ringhost@gmail.com>
2019-12-22 00:06:15 +0200

    done

--- paint 98/script.js
+++ paint 98/script.js
@@ -1,24 +1,57 @@
-var x = null;
-var y = null;
+const painter={};
 
+painter.color=["white", "black", "brown", "pink", 'purple', 'blue', 'lightblue', 'green', 'lightgreen', 'yellow', 'orange', 'red'];
 
-document.addEventListener('mousedown', onMouseUpdate, false);
-document.addEventListener('mouseup', onMouseUpdate, false);
+painter.insertColors=()=>{
+
+    for(const color of painter.color){
+        var newElement =document.createElement('div');
+        newElement.style.backgroundColor=color;
+        document.querySelector("#colors").appendChild(newElement);
+    }
+}
+
+painter.insertColors();
+
+var my_canvas=document.querySelector("#paintboard");
+var eraser = $("#white");
+var flag=true;
+
+painter.draw=()=>{
+
+    if(flag){
     
-function onMouseUpdate(e) {
-  x = e.pageX;
-  y = e.pageY;
+    var newDot=document.createElement('div');
+    document.body.appendChild(newDot);
+    newDot.style.position="absolute";
+    newDot.style.backgroundColor= "black"
+    choosecolor= ()=>{
+        if (eraser){
+            newDot.style.backgroundColor= "white";
+        } else{
+            newDot.style.backgroundColor= "black"
+            }
+    }
+    newDot.style.width="4px";
+    newDot.style.height="4px";
+    newDot.style.left=`${event.pageX}px`;
+    newDot.style.top=`${event.pageY}px`;
+    newDot.style.borderRadius = "50%"
 
-  console.log(x, y);
-  console.log(s);
-  
+    document.querySelector(".color").appendChild(newDot);
+        }
+    }
 
+painter.stop=()=>{
+
+flag=false;
 }
 
-function getMouseX() {
-  return x;
-}
+painter.start=()=>{
+    flag=true;
 
-function getMouseY() {
-  return y;
 }
+my_canvas.addEventListener('mousedown',()=>{my_canvas.addEventListener('mousemove', painter.draw)});
+my_canvas.addEventListener('mousemove', painter.draw);
+window.addEventListener('mouseup', painter.stop);
+window.addEventListener('mousedown', painter.start);
